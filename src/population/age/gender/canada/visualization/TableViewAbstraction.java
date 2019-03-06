535590737b9cfd51e95312ddbd4d4bd32d6b17bb
wang0561 <wang0561@algonquinlive.com>
2019-03-06 13:39:43 -0500

    Update TableViewAbstraction.java

--- src/population/age/gender/canada/visualization/TableViewAbstraction.java
+++ src/population/age/gender/canada/visualization/TableViewAbstraction.java
@@ -1,28 +1,60 @@
 package populcation.age.gender.canada.visualization;
 
 import java.util.List;
-
+import javafx.collections.ObservableList;
+import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
+import javafx.scene.control.cell.PropertyValueFactory;
 import populcation.age.gender.canada.PopulationMedian;
 import populcation.age.gender.canada.PopulationNum;
 
 public abstract class TableViewAbstraction<PopulationProperties> extends TableView<PopulationProperties> {
 
-	public TableViewAbstraction(String value, List<PopulationNum> numbers, List<PopulationMedian> medians) {
-		// TODO Auto-generated constructor stub
+	@SuppressWarnings("unchecked")
+	public TableViewAbstraction(String value, List<PopulationNum> numbers, 
+			List<PopulationMedian> medians,String[] colnames, String[] colValues
+			,int numofcol) {
+		
 		this.value = value;
-		this.numbers =numbers;
+		this.numbers = numbers;
 		this.medians = medians;
+		this.colnames = colnames;
+		this.colValues = colValues;
+		this.columns = new TableColumn[numofcol];
 	}
-	public TableViewAbstraction(int year, List<PopulationNum> numbers, List<PopulationMedian> medians) {
-		// TODO Auto-generated constructor stub
+
+	@SuppressWarnings("unchecked")
+	public TableViewAbstraction(int year, List<PopulationNum> numbers, 
+			List<PopulationMedian> medians,String[] colnames, String[] colValues
+			,int numofcol) {
+	
 		this.year = year;
-		this.numbers =numbers;
+		this.numbers = numbers;
 		this.medians = medians;
+		this.colnames = colnames;
+		this.colValues = colValues;
+		this.columns = new TableColumn[numofcol];
 	}
-	List<PopulationNum> numbers;
-	List<PopulationMedian> medians;
-	String value;
-	int year;
-	public abstract void initialization();
+
+	protected List<PopulationNum> numbers;
+	protected List<PopulationMedian> medians;
+	protected String value;
+	protected int year;
+	protected TableColumn<PopulationProperties,?>[] columns ;
+	protected String[] colnames ;
+	protected String[] colValues;
+	
+
+	public void initialization() {
+		for(int i=0; i<columns.length;i++) {
+			columns[i]= new TableColumn<>(colnames[i]);
+			columns[i].setMinWidth(200);
+			columns[i].setCellValueFactory(new PropertyValueFactory<>(colValues[i]));
+			columns[i].setStyle("-fx-alignment: CENTER;");
+			this.getColumns().add(columns[i]);
+		}
+		this.setItems(getObservableList());
+	}
+	
+	public abstract ObservableList<PopulationProperties> getObservableList();
 }
