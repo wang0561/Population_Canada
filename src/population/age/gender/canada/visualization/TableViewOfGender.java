7a97d701e1b26e072327595a2c72a7e5737b4f90
wang0561 <wang0561@algonquinlive.com>
2019-03-06 13:41:29 -0500

    Update TableViewOfGender.java

--- src/population/age/gender/canada/visualization/TableViewOfGender.java
+++ src/population/age/gender/canada/visualization/TableViewOfGender.java
@@ -4,52 +4,38 @@ import java.util.List;
 import java.util.stream.Collectors;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
-import javafx.scene.control.TableColumn;
-import javafx.scene.control.cell.PropertyValueFactory;
 import populcation.age.gender.canada.PopulationMedian;
 import populcation.age.gender.canada.PopulationNum;
 
 public class TableViewOfGender<PopulationProperties> extends TableViewAbstraction<PopulationProperties> {
-	
-	@SuppressWarnings("unchecked")
-	TableColumn<PopulationProperties,?>[] columns = new TableColumn[5];
-	final String[] colnames = {"Year","Geo", "AgeGroup", "Number","Median"};
-	final String[] colValues = {"Year","Geo","Agegroup","Number","Median"};
-	
-	public TableViewOfGender(String value, List<PopulationNum> numbers, List<PopulationMedian> medians) {
-		
-		super(value,numbers,medians);
+
+	public TableViewOfGender(String value, List<PopulationNum> numbers, List<PopulationMedian> medians,
+			String[] colnames, String[] colValues, int num) {
+
+		super(value, numbers, medians, colnames, colValues, num);
 		initialization();
 	}
 
-	@Override
-	public void initialization() {
-		
-		for(int i=0; i<columns.length;i++) {
-			columns[i]= new TableColumn<>(colnames[i]);
-			columns[i].setMinWidth(200);
-			columns[i].setCellValueFactory(new PropertyValueFactory<>(colValues[i]));
-			columns[i].setStyle("-fx-alignment: CENTER;");
-			this.getColumns().add(columns[i]);
-		}
-		this.setItems(getObservableList());
-	}
-
 	@SuppressWarnings("unchecked")
-	private ObservableList<PopulationProperties> getObservableList() {
+	public ObservableList<PopulationProperties> getObservableList() {
 
 		ObservableList<PopulationProperties> data = FXCollections.observableArrayList();
-		List<PopulationNum> nums = numbers.stream().filter(e-> e.getGender().equals(value)).collect(Collectors.toList());
-		List<PopulationMedian> meds = medians.stream().filter(e->e.getGender().equals(value)).collect(Collectors.toList());
-		
-		for(PopulationNum populationNum : nums ) {
-			
-			PopulationMedian popmed = meds.stream().filter(e->e.getYear() == populationNum.getYear() && 
-					e.getGEO().equals(populationNum.getGEO())).collect(Collectors.toList()).get(0);
-			
-			data.add((PopulationProperties) new populcation.age.gender.canada.properties_table_column.PopulationProperties(populationNum.getGEO(),populationNum.getYear(),populationNum.getAgeGroup(),
-					popmed.getMed(),populationNum.getNum()));
-			
+		List<PopulationNum> nums = numbers.stream().filter(e -> e.getGender().equals(value))
+				.collect(Collectors.toList());
+		List<PopulationMedian> meds = medians.stream().filter(e -> e.getGender().equals(value))
+				.collect(Collectors.toList());
+
+		for (PopulationNum populationNum : nums) {
+
+			PopulationMedian popmed = meds.stream()
+					.filter(e -> e.getYear() == populationNum.getYear() && e.getGEO().equals(populationNum.getGEO()))
+					.collect(Collectors.toList()).get(0);
+
+			data.add(
+					(PopulationProperties) new populcation.age.gender.canada.properties_table_column.PopulationProperties(
+							populationNum.getGEO(), populationNum.getYear(), populationNum.getAgeGroup(),
+							popmed.getMed(), populationNum.getNum()));
+
 		}
 		return data;
 	}
