ca0392ca48ebb1882cebc71eab0f30c87a645327
wang0561 <wang0561@algonquinlive.com>
2019-03-06 13:38:33 -0500

    Create ColumnsArray.java

---
+++ src/population/age/gender/canada/visualization/ColumnsArray.java
@@ -0,0 +1,18 @@
+package populcation.age.gender.canada.visualization;
+
+public class ColumnsArray {
+	
+	public static final String[] AGE_NAME = {"Year","Geo", "Gender", "Number"};
+	public static final String[] AGE_VALUE = {"Year","Geo","Gender","Number"};
+	
+	public static final String[] GENDER_NAME = {"Year","Geo", "AgeGroup", "Number","Median"};
+	public static final String[] GENDER_VALUE = {"Year","Geo","Agegroup","Number","Median"};
+ 
+	public static final String[] GEO_NAME= {"Year","Gender", "AgeGroup", "Number","Median"};
+	public static final String[] GEO_VALUE= {"Year","Gender","Agegroup","Number","Median"};
+							
+	public static final String[] YEAR_NAME= {"Geo","Gender", "AgeGroup", "Number","Median"};
+	public static final String[] YEAR_VALUE= {"Geo","Gender","Agegroup","Number","Median"};
+
+
+}
